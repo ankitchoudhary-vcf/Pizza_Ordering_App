cea94b180331166b245cf9a6ea5553abe7680edf
ankit <ac2000choudhary@gmail.com>
2022-04-20 22:11:30 +0530

    User Module Added to App Module

--- function/src/app.module.ts
+++ function/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
