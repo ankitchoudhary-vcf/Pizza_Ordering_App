922f3070df954dd5faaf5681e5c116470944e003
ankit <ac2000choudhary@gmail.com>
2022-04-21 07:17:39 +0530

    Data Transfer Object schema

---
+++ function/src/modules/ingredients/dto/ingredients.dto.ts
@@ -0,0 +1,17 @@
+// Ingredients Data Transfer Object schema.
+import { IsNotEmpty } from 'class-validator'
+
+export class IngredientsDto {
+
+    @IsNotEmpty()
+    readonly Name: string;
+
+    @IsNotEmpty()
+    readonly Type: string;
+
+    @IsNotEmpty()
+    readonly Price: number;
+
+    @IsNotEmpty()
+    readonly AvatarURL: string;
+}
