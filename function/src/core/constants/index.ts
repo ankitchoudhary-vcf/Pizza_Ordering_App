28848e966abd60e0afb4ec9a529f735ac6648d1c
ankit <ac2000choudhary@gmail.com>
2022-04-22 10:49:56 +0530

    Constant Added

--- function/src/core/constants/index.ts
+++ function/src/core/constants/index.ts
@@ -4,3 +4,5 @@ export const TEST = 'test';
 export const PRODUCTION = 'production';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
 export const INGREDIENT_REPOSITORY = 'INGREDIENT_REPOSITORY'
+export const CART_REPOSITORY = 'CART_REPOSITORY'
+export const CARTITEM_REPOSITORY ='CARTITEM_REPOSITORY'
