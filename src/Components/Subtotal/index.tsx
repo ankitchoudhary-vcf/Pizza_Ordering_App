5c50dca1b476c49db47c8eae2a01a2f800f56c44
ankit <ac2000choudhary@gmail.com>
2022-04-24 23:48:14 +0530

    SubTotal Component Created

---
+++ src/Components/Subtotal/index.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./style.css";
+import { useStateValue } from "../../Services/StateProvider";
+import { useNavigate } from "react-router-dom";
+import axiosConfig from "../../Services/axios/axios";
+import { showToast } from '../../Services/Reducer/index';
+import warning from '../../Assets/Img/warning.svg'
+import success from '../../Assets/Img/check.svg'
+
+const Subtotal = () => {
+  const [state, dispatch] = useStateValue();
+  const navigate = useNavigate();
+
+  const order = (e: any) => {
+    e.preventDefault();
+
+    // Adding the authorization Bearer Token to the api configuration.
+    axiosConfig.interceptors.request.use(
+      (config: any) => {
+        config.headers.authorization = `Bearer ${state.user.token}`;
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+    var err = false;
+
+    state.basket.map(async (item: any) => {
+      const orderItems = {
+        Price: item.Price,
+        OrderItem: item.CartItems.map((item: any) => ({
+          IngredientId: item.IngredientId,
+        })),
+      };
+
+      await axiosConfig
+        .post(`/api/orders/add`, orderItems)
+        .then((response) => {
+        //   console.log(response);
+        })
+        .catch((error) => {
+            err = error.message;
+        });
+    });
+
+    if(err){
+        showToast('warning', dispatch, {
+            title: err,
+            image: warning,
+          })
+    }
+    else{
+        showToast('success', dispatch, {
+            title: "Order Placed Successfully",
+            image: success,
+          })
+          navigate('/orders');
+    }
+
+  };
+
+  const getBasketTotal = (basket: any) => {
+    return basket?.reduce((Price: any, item: any) => item.Price + Price, 0);
+  };
+
+  return (
+    <div className="subtotal">
+      <div className="subtotal_info">
+        <strong>Total Amount : </strong> ₹ <>{getBasketTotal(state.basket)}</>
+      </div>
+      <button onClick={(e) => (state.user ? order(e) : navigate("/login"))}>
+        Proceed to Checkout
+      </button>
+    </div>
+  );
+};
+
+export default Subtotal;
